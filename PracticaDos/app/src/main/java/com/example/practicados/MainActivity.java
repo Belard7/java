d7cc2c7d9edfa390ffda407d4701ddb2df8d3302
Jimmy <abelardorivas568@gmail.com>
2024-01-29 07:47:36 -0600

    primer comentario

---
+++ PracticaDos/app/src/main/java/com/example/practicados/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.practicados;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu opcion_menu) {
+        getMenuInflater().inflate(R.menu.menu, opcion_menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem opcion_menu) {
+        int id = opcion_menu.getItemId();
+        if (id == R.id.configuracion) {
+            Toast.makeText(this, "Configuracion", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.informacion) {
+            Toast.makeText(this, "Informacion", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+     return super.onOptionsItemSelected(opcion_menu);
+    }
+}
