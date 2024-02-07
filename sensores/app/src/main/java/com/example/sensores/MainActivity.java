49db08e986ac468c45b8bbffc97e5d2fe0c8b511
Jimmy <abelardorivas568@gmail.com>
2024-02-07 09:29:36 -0600

    second

--- sensores/app/src/main/java/com/example/sensores/MainActivity.java
+++ sensores/app/src/main/java/com/example/sensores/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.sensores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaracion de variables
+
+    SensorManager sensorManager;
+
+    Sensor sensor;
+
+    SensorEventListener sensorEventListener;
+
+    int whip = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        final TextView sonido = (TextView) findViewById(R.id.tvSonido);
+
     }
 }
